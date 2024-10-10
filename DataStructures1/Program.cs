15874c9675b30f90abf545c228bfcf555498cef8
realwizz <womiowuna@gmail.com>
2024-10-10 10:58:46 +0100

    created an indefinite loop using additional methods for users to choose what they'd like to view

--- DataStructures1/Program.cs
+++ DataStructures1/Program.cs
@@ -10,18 +10,26 @@ namespace DataStructures1
     {
         static void Main(string[] args)
         {
+
+            MainMenu();
+
+            Console.ReadKey();
+        }
+
+        private static void ListMovies()
+        {
             Movie deadpool3;
             deadpool3.title = "Deadpool & Wolverine";
             deadpool3.releaseYear = 2024;
             deadpool3.price = 19.99;
             deadpool3.rating = MovieRating._15;
 
+            Console.Clear();
             Console.WriteLine(deadpool3.title + " (" + deadpool3.releaseYear + ") Rated " + deadpool3.rating);
+        }
 
-            Console.ReadKey();
-            Console.WriteLine("\n\n");
-            //Console.Clear();
-
+        private static void ListShows()
+        {
             List<TVShow> shows = new List<TVShow>();
             shows.Add(new TVShow("The Lord of the Rings: The Rings of Power", 2022, 0, MovieRating._15));
             shows.Add(new TVShow("The Boys", 2019, 7.99, MovieRating._18));
@@ -31,8 +39,42 @@ namespace DataStructures1
             {
                 Console.WriteLine(shows[i].ToString());
             }
+        }
 
-            Console.ReadKey();
+        private static void MainMenu()
+        {
+            //Infinite loop (until application is exited)
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please choose from thye following options: \n");
+                Console.WriteLine("(1) List Movies");
+                Console.WriteLine("(2) List TVShows");
+                Console.WriteLine("\n");
+                Console.WriteLine("(0) Exit Application");
+                Console.WriteLine("\n");
+
+                string userInput = Console.ReadLine();
+
+                //Verify if the user has entered an integer value.
+                if (int.TryParse(userInput, out int integerInput))
+                {
+                    switch (integerInput)
+                    {
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+                        case 1:
+                            ListMovies();
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            ListShows();
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+            }
         }
 
         public enum MovieRating
